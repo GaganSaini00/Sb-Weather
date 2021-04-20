7f7ef32a1210d8d539c89ff19963b2be5ce804a4
Jp-CENG319 <72248164+Jp-CENG319@users.noreply.github.com>
2021-04-19 19:17:47 -0700

    Milestone 3

---
+++ Sb Weather Milestone 3/SB Weather/sb weather/src/main/java/weather/app/simpleweather/Temperature_Model.java
@@ -0,0 +1,18 @@
+package weather.app.simpleweather;
+//Sunshine Boys
+
+public class Temperature_Model {
+
+    private Integer Temperature;
+
+    public Temperature_Model(){}
+
+
+    public Integer getTemperature() {
+        return Temperature;
+    }
+
+    public void setTemperature(Integer temperature) {
+        Temperature = temperature;
+    }
+}
